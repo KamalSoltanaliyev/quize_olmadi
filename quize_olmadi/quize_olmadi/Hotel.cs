4988b06dd466022e9192e0a6d10f178bb8dc201a
KamalSoltanaliyev <148267364+KamalSoltanaliyev@users.noreply.github.com>
2023-11-29 19:03:57 +0400

    Add files via upload

---
+++ quize_olmadi/quize_olmadi/Hotel.cs
@@ -0,0 +1,56 @@
+﻿namespace quize_olmadi
+{
+    public class Hotel
+    {
+        private string name;
+        private Room[] rooms;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("duz deyil xiyar, cix cole!");
+                }
+                name = value;
+            }
+        }
+
+        public Hotel(string name)
+        {
+            Name = name;
+            rooms = new Room[0];
+        }
+        public void MakeReservation(int roomId)
+        {
+            try
+            {
+                if (roomId == null)
+                {
+                    throw new NullReferenceException("room null ola bilmez, xiyar");
+                }
+
+                Room room = rooms.FirstOrDefault(r => r.Id == roomId);
+
+                if (room == null)
+                {
+                    throw new ArgumentException($"roomun bele id {roomId} tapilmayib, xiyar");
+                }
+
+               
+                room.IsAvailable = false;
+                Console.WriteLine($"Reservation made for Room {roomId}");
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ quize_olmadi/quize_olmadi/Room.cs
@@ -0,0 +1,33 @@
+﻿namespace quize_olmadi
+{
+    public class Room
+    {
+        private static int counter = 1;
+        public int Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public int PersonCapacity { get; }
+        public bool IsAvailable { get; set; } = true;
+
+        public Room (int id, string name, decimal price, int personCapacity)
+        {
+            Id = counter++;
+            Name = name;
+            Price = price;
+            PersonCapacity = personCapacity;
+        }
+        public string ShowInfo()
+        {
+            return $"Id: {Id}, Name: {Name}, Price: {Price}, Capacity: {PersonCapacity}, Available: {IsAvailable}";
+        }
+        public override string ToString()
+        {
+            return ShowInfo();
+        }
+    }
+}
+
+
+
+
+
